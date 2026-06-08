0a05a1f0a2cd23fd4afe1cc892c146fc909f95af
Merrin W. Dream <post@rimmerworld.info>
2026-06-08 20:37:41 +0100

    Add voice shape control

---
+++ shape-patch.js
@@ -0,0 +1,15 @@
+state.shape='pure';
+labels.pure='Pure';
+labels.hollow='Hollow';
+labels.pressed='Pressed';
+
+const hasShapeControl=playDefs.some(([key])=>key==='shape');
+if(!hasShapeControl){
+  playDefs.unshift([
+    'shape',
+    'Shape',
+    'Voice source character',
+    [['pure','Pure'],['hollow','Hollow'],['pressed','Pressed']]
+  ]);
+  renderAll();
+}
